a4d3edcea16f4e7ef2c1e930d82d52979fca9b49
Taras Shapovalov <tarasshapovalov85@gmail.com>
2022-06-09 23:21:46 +0300

    not finish task

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,19 +2,11 @@ export { fetchCountries };
 import Notiflix from 'notiflix';
 
 function fetchCountries(name) {
-    // name
-    fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
         .then(response => {
             if (!response.ok) {
-                throw new Error(response.status);
+                throw new Error(Notiflix.Notify.failure("Oops, there is no country with that name"))
             }
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-            return data;
-        })
-        .catch(error => {
-            // Error handling
+           response.json();
         })
 };
--- src/index.js
+++ src/index.js
@@ -8,26 +8,38 @@ const refs = {
     listCountry: document.querySelector('.country-list'),
     cardCountry: document.querySelector('.country-info'),
 }
-refs.input.addEventListener('input', inputReader);
+refs.input.addEventListener('input', debounce(inputReader, DEBOUNCE_DELAY));
 
-// const newName = debounce(fetchCountries, DEBOUNCE_DELAY);
 function inputReader(e) {
-    const nameValue = e.target.value;
+    const name = e.target.value.trim();
+    if (name === "") {
+    return;
+  }
+  fetchCountries(name)
+      .then(response => {
+      if (response.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      }
+        if (response.length === 1) {
+          renderContryCard({ response });
+      }
+    })
+    .catch(error => console.log(error));
+//   clearSearchCountry();
     // console.log(name);
-    const country = fetchCountries(nameValue);
-    // console.log(country);
-    
-    
-    renderContryCard(country)
-    // newName();
+    // const countryAll = fetchCountries(name);
+    // renderContryCard(countryAll);
+           
 };
-
-function renderContryCard() {
-    const markup = (country) => {
+function renderContryCard(country) {
+    const {name,capital,population,flags,languages} = country
+    const markup = ({ name, capital, population, flags, languages }) => {
         return `<h1 class="country"><img src="${flags.png}" alt="flag">${name.official}</h1>
-//     <p class="capital">Capital: ${capital}</p>
-//     <p class="population">Population: ${population}</p>
-//     <p class="languages">Languages: ${languages}</p>`
-    }
+    <p class="capital">Capital: ${capital}</p>
+    <p class="population">Population: ${population}</p>
+    <p class="languages">Languages: ${languages}</p>`
+    };
     refs.cardCountry.innerHTML = markup;
 };
